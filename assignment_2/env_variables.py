205d2b1eeb1417f102b89e69ea371e3cc2e1880d
Anh Dang <anh.dang@amanotes.com>
2021-05-11 00:10:49 +0700

    Create env_variables.py
    Add py file for env_variables

---
+++ assignment_2/env_variables.py
@@ -0,0 +1,5 @@
+import os
+os.environ['SENDER_EMAIL'] = 'abc' ## INPUT: Your Email
+os.environ['PWD_EMAIL'] = '1234' ## INPUT: Your App Password
+os.environ['WEATHER_API_KEY] = '*********' ## INPUT: OpenWeather API Key
+print('DONE! Get Env. Variable')
